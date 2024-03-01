ac589a3143bef5919ec35d80ac3f5359a1b546fe
sayya <djdj9876@naver.com>
2024-03-01 18:55:13 +0900

    project init

---
+++ src/main/java/com/example/daymateproject/DayMateProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.daymateproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayMateProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DayMateProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/daymateproject/DayMateProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.daymateproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayMateProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
